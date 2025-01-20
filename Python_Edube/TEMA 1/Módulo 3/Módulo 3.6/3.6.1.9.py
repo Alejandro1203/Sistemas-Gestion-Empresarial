00d273b7c5262b93f8da78acba0e84059ca0bea3
Alejandro1203 <alefg2003@gmail.com>
2025-01-20 09:28:21 +0100

    Módulo 3.6

--- Python_Edube/TEMA 1/Módulo 3/Módulo 3.6/3.6.1.9.py
+++ Python_Edube/TEMA 1/Módulo 3/Módulo 3.6/3.6.1.9.py
@@ -1,11 +1,11 @@
 my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
 sinDuplicados = []
 
-for i in my_list:
-    if i not in sinDuplicados:
-        sinDuplicados.append(i)
+for index in my_list:
+    if index not in sinDuplicados:
+        sinDuplicados.append(index)
 
 
-print("La lista con elementos únicos:" + str(sinDuplicados))
-print("La lista con elementos únicos:" + str(set(my_list)))
+print("La lista con elementos únicos:", sinDuplicados)
+# print("La lista con elementos únicos:", set(my_list))
 print(my_list)
