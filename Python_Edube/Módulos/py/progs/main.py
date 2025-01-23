6f34f1ebb8cf6c196ba0170a7707b30771b84124
Alejandro1203 <alefg2003@gmail.com>
2025-01-23 12:02:03 +0100

    Modulos

---
+++ Python_Edube/Módulos/py/progs/main.py
@@ -0,0 +1,11 @@
+from sys import path
+
+path.append('..\\modules')
+# path.append('C:\\Users\ALUMNO2425\Documents\GitHub\Sistemas-Gestion-Empresarial\Python_Edube\Módulos\py\modules')
+
+import modulo
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(modulo.suml(zeroes))
+print(modulo.prodl(ones))
