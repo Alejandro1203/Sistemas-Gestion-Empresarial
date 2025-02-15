5d3c2c0e073dff63689a63e43a190edb00e0bd88
Alejandro1203 <alefg2003@gmail.com>
2025-02-15 14:50:00 +0100

    Módulo 1.3 / mi primer módulo

---
+++ Python_Edube/TEMA_2/Modulo_1/Modulo_1.3/mi_primer_modulo/progs/main2.py
@@ -0,0 +1,11 @@
+from packages.extra.iota import funI
+from packages.extra.good.best.sigma import funS
+from packages.extra.good.best.tau import funT
+from packages.extra.good.alpha import funA
+from packages.extra.good.beta import funB
+
+print(funI())
+print(funS())
+print(funT())
+print(funA())
+print(funB())
